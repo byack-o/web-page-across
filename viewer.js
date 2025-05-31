e9e6e013cf3136183da4414380ae76bb33a65043
ryunosuke fujii <fujii8848ryu@icloud.com>
2025-06-01 03:01:41 +0900

    最初の角度からの差

--- viewer.js
+++ viewer.js
@@ -1,16 +1,24 @@
-//const layerDepths = [100, 200, 400]
-
 function handleOrientation(event) {
   //const alpha = event.alpha; // 端末の回転（方位）
-  const beta = event.beta;   // 前後の傾き
-  const gamma = event.gamma; // 左右の傾き
+  const beta = event.beta ?? 0;   // 前後の傾き（上下）
+  const gamma = event.gamma ?? 0; // 左右の傾き（左右）
+
+    // 初回に基準角を記録
+  if (baseBeta === null || baseGamma === null) {
+    baseBeta = beta;
+    baseGamma = gamma;
+  }
+
+  // 相対角度を算出（現在 - 基準）
+  const deltaBeta = beta - baseBeta;
+  const deltaGamma = gamma - baseGamma;
 
   const images = document.querySelectorAll('.image-stack img')
 
   images.forEach((img, i) => {
     const depth = layerDepths[i] || 1.0;
-    const moveX = -Math.tan(gamma * (Math.PI / 180)) * depth;
-    const moveY = Math.tan(beta * (Math.PI / 180)) * depth;
+    const moveX = -Math.tan(deltaGamma * (Math.PI / 180)) * depth;
+    const moveY = Math.tan(deltaBeta * (Math.PI / 180)) * depth;
 
     img.style.transform = `translate(${moveX}px, ${moveY}px)`;
     //img.style.transform = `translate(${moveX}px, 0px)`;
