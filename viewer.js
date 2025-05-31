06be2fabb808cbdd56e2c91533bdde470018effb
ryunosuke fujii <fujii8848ryu@icloud.com>
2025-06-01 02:50:36 +0900

    depthを引数で与える

--- viewer.js
+++ viewer.js
@@ -1,8 +1,8 @@
-const layerDepths = [100, 200, 400]
+//const layerDepths = [100, 200, 400]
 
 function handleOrientation(event) {
   //const alpha = event.alpha; // 端末の回転（方位）
-  //const beta = event.beta;   // 前後の傾き
+  const beta = event.beta;   // 前後の傾き
   const gamma = event.gamma; // 左右の傾き
 
   const images = document.querySelectorAll('.image-stack img')
@@ -10,10 +10,10 @@ function handleOrientation(event) {
   images.forEach((img, i) => {
     const depth = layerDepths[i] || 1.0;
     const moveX = -Math.tan(gamma * (Math.PI / 180)) * depth;
-    //const moveY = Math.tan(beta * (Math.PI / 180)) * depth;
+    const moveY = Math.tan(beta * (Math.PI / 180)) * depth;
 
-    //img.style.transform = `translate(${moveX}px, ${moveY}px)`;
-    img.style.transform = `translate(${moveX}px, 0px)`;
+    img.style.transform = `translate(${moveX}px, ${moveY}px)`;
+    //img.style.transform = `translate(${moveX}px, 0px)`;
   });
 
   
