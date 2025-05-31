f722f4a05ae5057bf50ba5217fe47174c72121f3
ryunosuke fujii <fujii8848ryu@icloud.com>
2025-06-01 03:25:57 +0900

    add the work astaroth

--- scripts.js
+++ scripts.js
@@ -44,6 +44,29 @@ function initOrientation() {
   }
 }
 
+function initOrientationAstaroth() {
+  if (
+    typeof DeviceOrientationEvent !== 'undefined' &&
+    typeof DeviceOrientationEvent.requestPermission === 'function'
+  ) {
+    // iOS 13以降：ユーザーの許可が必要
+    DeviceOrientationEvent.requestPermission()
+      .then(permissionState => {
+        if (permissionState === 'granted') {
+          window.location.href = 'astaroth.html';
+        } else {
+          alert('センサーの使用が許可されませんでした。');
+        }
+      })
+      .catch(err => {
+        console.error('センサー許可リクエストエラー:', err);
+      });
+  } else {
+    // Androidなどはそのまま使える
+    window.location.href = 'astaroth.html';
+  }
+}
 
 // ボタンにイベント登録
 document.getElementById('startOrientation').addEventListener('click', initOrientation);
+document.getElementById('startOrientationAstaroth').addEventListener('click', initOrientationAstaroth);
